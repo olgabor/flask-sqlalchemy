cbae14af4263a3d98e3515f53509ab8601431ad9
Olb2 <olgaborysovska@gmail.com>
2020-09-08 14:12:15 -0700

    adds two routes to api.py and errorhandler

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from models import app, User
 from flask import jsonify
+from crud.user_crud import get_all_users, get_user 
 
 @app.route('/')
 def home():
@@ -7,4 +8,18 @@ def home():
   print(f'🎀 {first_user}')
   return jsonify(user=first_user.as_dict())
 
+#Routes 
+@app.route('/users/')
+def user_index_create(): 
+    return get_all_users()
 
+@app.route('/users/<int:id>')
+def user_show_put_delete(id):
+    return get_user(id)
+
+#error handler 
+@app.errorhandler(Exception) 
+def unhandeled_error(e):
+    app.logger.error('Unhandeled Exception: %s' , (e))
+    message_str = e.__str__()
+    return jsonify(message=message_str.split(':')[0])
